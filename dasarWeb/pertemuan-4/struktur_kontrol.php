2cda355e5a92a4df9f7bec13f1ded2a5b399129a
FaliIrham <falimagemaulana64@gmail.com>
2024-09-20 09:09:30 +0700

    Praktikum Bagian 4 Soal No 4.2 & 4.3

--- dasarWeb/pertemuan-4/struktur_kontrol.php
+++ dasarWeb/pertemuan-4/struktur_kontrol.php
@@ -1,8 +1,9 @@
 <?php 
+//Soal No 4.1 
 $nilaiNumerik = 92;
 
 if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
-    echo "Nilai Huruf: A";
+    //echo "Nilai Huruf: A";
 }elseif($nilaiNumerik >= 80 && $nilaiNumerik < 90){
     echo "Nilai Huruf: B";
 }elseif($nilaiNumerik >= 70 && $nilaiNumerik < 80){
@@ -10,7 +11,7 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 }elseif($nilaiNumerik < 70){
     echo "Nilai Huruf: D";
 }
-
+//Soal No 4.2 
 $jarakSaatIni = 0;
 $jarakTarget = 500;
 $peningkatanHarian = 30;
@@ -20,6 +21,16 @@ while ($jarakSaatIni < $jarakTarget) {
     $jarakSaatIni += $peningkatanHarian;
     $hari++;
 }
+//echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 Kilometer";
 
-echo "<h2>Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 Kilometer</h2>";
+//Soal No 4.3
+$jumlahLahan = 10;
+$tanamanPerlahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for($i = 1; $i <= $jumlahLahan;$i ++){
+    $jumlahBuah += ($tanamanPerlahan + $buahPerTanaman);
+}
+echo "<h2>Jumlah buah yang akan dipanen adalah : $jumlahBuah </h2>";
 ?>
