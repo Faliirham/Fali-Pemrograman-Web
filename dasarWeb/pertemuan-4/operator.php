b1f6929ef384f2bd5a8fe3331afef7a0f7e47e0c
FaliIrham <falimagemaulana64@gmail.com>
2024-09-20 08:22:00 +0700

    Praktikum Bagian 3 Soal No 3.2

--- dasarWeb/pertemuan-4/operator.php
+++ dasarWeb/pertemuan-4/operator.php
@@ -9,12 +9,11 @@
     $sisaBagi = $a % $b ;
     $pangkat = $a ** $b;
 
-    
     $hasilSama = $a == $b;
     $hasilTidakSama = $a != $b;
     $hasilLebihKecil = $a < $b;
     $hasilLebihBesar = $a > $b;
     $hasilLebihKecilSama = $a <= $b;
     $hasilLebihBesarSama = $a >= $b;
-    
+
 ?>
