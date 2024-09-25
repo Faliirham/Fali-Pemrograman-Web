d6837dbf3f8d2a92d5f098123d6e203d9a179530
FaliIrham <falimagemaulana64@gmail.com>
2024-09-25 21:13:47 +0700

    Praktikum Bagian 5 Soal No 5,3

--- dasarWeb/pertemuan-4/array.php
+++ dasarWeb/pertemuan-4/array.php
@@ -11,7 +11,7 @@ foreach ($nilaiSiswa as $nilai) {
     } 
 }
 
-//echo "Daftar nilai siswa yang lulus : " . implode (',', $nilaiLulus);
+echo "Daftar nilai siswa yang lulus : " . implode (',', $nilaiLulus);
 
 // Soal No 5.2
 
@@ -33,4 +33,32 @@ foreach ($daftraKaryawan as $karyawan) {
 
 echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 Tahun: " . implode (', ', 
 $karyawanPengalamanLimaTahun);
+
+// Soal No 5.3
+
+$daftarNilai = [
+    'Matematika' => [
+        ['Alice', 85],
+        ['Bob', 92],
+        ['Charlie', 78],
+    ],
+    'Fisika' => [
+        ['Alice', 90],
+        ['Bob', 88],
+        ['Alice', 75],
+    ],
+    'Kimia' =>[
+        ['Alice', 92],
+        ['Bob', 80],
+        ['Charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+
+echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
+
+foreach($daftarNilai[$mataKuliah] as $nilai){
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
 ?>
