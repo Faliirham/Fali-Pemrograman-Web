9c2dd3507d9a015ac8d052609f729fbc1e996e1a
FaliIrham <falimagemaulana64@gmail.com>
2024-10-10 12:47:48 +0700

    Praktikum Bagian 1 Soal no 1.2

--- dasarWeb/pertemuan-7/isset.php
+++ dasarWeb/pertemuan-7/isset.php
@@ -1,9 +1,19 @@
 <?php
+//Soal No 1.1
 $umur;
 if(isset($umur)&& $umur >=18){
     echo"Anda Sudah Dewawas";
 }else{
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+}
+
+echo "<br>";
+//Soal No 1.2
+$data = array("nama" => "Jane", "usia"=> 25);
+if (isset($data["nama"])) {
+    echo "Nama: ". $data["nama"];
+}else{
+    echo "Variabel 'nama' tidak ditentukan dakam array.";
 }
 ?>
 
