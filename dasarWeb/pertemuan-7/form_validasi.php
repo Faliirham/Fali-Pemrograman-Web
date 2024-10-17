58c91ef15a390721c63ee00d9706579a7485e949
FaliIrham <falimagemaulana64@gmail.com>
2024-10-17 11:28:54 +0700

    Praktikum Bagian 7 Soal no 7.2

--- dasarWeb/pertemuan-7/form_validasi.php
+++ dasarWeb/pertemuan-7/form_validasi.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Form Input dengan Validasi</title>
+        <script src = "https://code.querry.com/jquerry-3.6.0.min.js"></script>
     </head>
     <body>
         <h1> Form input dengan Validasi</h1>
@@ -16,5 +17,33 @@
 
             <input type="submit" value="Submit">
         </form>
+
+        <script>
+            $(document).ready(function() {
+                $("myForm").submit(function(event){
+                    var nama = $('#nama').val();
+                    var email = $('#email').val();
+                    var valid = true;
+                    
+                    if (nama === "") {
+                        $('#nama-error').text("Nama harus diisi.");
+                        valid = false;
+                    }else{
+                        $('#nama-error').text("");
+                    }
+
+                    if (email === "") {
+                        $('#email-error').text("Email harus diisi.");
+                        valid = false;
+                    } else{
+                        $('#email-error').text("");
+                    }
+
+                    if (valid) {
+                        (!event.preventDefault());x
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
