8d97e54d6589fc5580689910de4d2bca33f07b2f
FaliIrham <falimagemaulana64@gmail.com>
2024-10-17 09:52:15 +0700

    Praktikum Bagian 4 Soal no 4.2

--- dasarWeb/pertemuan-7/html_aman.php
+++ dasarWeb/pertemuan-7/html_aman.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>HTML INJECTION</title>
+    <title>Input Aman htmlspecialchars</title>
 </head>
 <body>
     <h2>Form Input PHP Aman</h2>
@@ -9,15 +9,26 @@
         <label for="input">Masukkan Inputan:</label>
         <input type="text" name="input" id="input" required><br><br>
 
+        <label for="email">Masukkan email:</label>
+        <input type="email" name="email" id="email" required><br><br>
+
         <input type="submit" value="Submit">
     </form>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+      
         $input = $_POST['input'];
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
         echo "Input yang diterima: " . $input . "<br>";
+
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+            echo "Email yang diterima: " . $email;
+        } else {
+            echo "Input email tidak valid.";
+        }
     }
     ?>
 </body>
